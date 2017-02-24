c8d4f62b915877893accf5d8008d3ccd6cfe6a4b
hansz <hansz07@163.com>
2017-02-24 14:52:40 +0800

    test class

---
+++ src/githubtest/StringTest.java
@@ -0,0 +1,12 @@
+package githubtest;
+
+
+public class StringTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println(String.format("%d+%d",1,2));
+	}
+
+}
